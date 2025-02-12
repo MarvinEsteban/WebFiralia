b65f328592b1411cf841dece661c938bb440d5d2
MarvinEsteban <tommyesgon0202@gmail.com>
2025-02-12 09:28:35 +0100

    Changed syles name to login.css

--- loginPrueba.php
+++ loginPrueba.php
@@ -34,7 +34,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Iniciar Sesión</title>
-    <link rel="stylesheet" href="syles.css">
+    <link rel="stylesheet" href="login.css">
 </head>
 <body>
     <div class="login-container">
