9ccd390d8923897e409318e6184545984529ab16
Joan Marc <joanmarcmartinezmot@gmail.com>
2025-02-28 08:13:28 +0100

    commit

--- controller/UserController.php
+++ controller/UserController.php
@@ -2,45 +2,16 @@
 session_start();
 
 
-$usuario_valido = 'usuario';
-$contrasena_usuario_valida = '12345';
-$admin_valido = 'admin';
-$contrasena_admin_valida = '54321';
-
-
 function login(){
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $user= $_POST['usuario'];
-    $password= $_POST['password'];
 
-
-    
-}
 }
 
 function logout(){
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $user= $_POST['usuario'];
-        $password= $_POST['password'];
-    
-        
-        
-    }
     }
 
     function register(){
 
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            $name= $_POST['name'];
-            $lastname= $_POST['lastname'];
-            $email= $_POST['email'];
-            $user= $_POST['user'];
-            $password= $_POST['password'];
-        
-            
-            
-        }
         }
 ?>
